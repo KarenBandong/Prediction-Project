8e7d97ddb247565ed3b27465e3771cd5efcdcbf0
Karen <karen.bandong.2018@student.lhschools.org>
2019-04-01 20:13:50 -0400

    Updated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,33 +7,47 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        int month, day, year;
+        int month, year, day;
+        boolean running = true;
+        while (running = true) {
 
-        System.out.println("What is the month you were born?");
-        month = keyboard.nextInt();
-        System.out.println("What day of the month were you born?");
-        day = keyboard.nextInt();
-        System.out.println("What year were you born?");
-        year = keyboard.nextInt();
-        System.out.println(monthoftheyear(month));
 
+        System.out.println("Monday's child is fair of face,\n" +
+                "Tuesday's child is full of grace,\n" +
+                "Wednesday's child is full of woe,\n" +
+                "Thursday's child has far to go.\n" +
+                "Friday's child is loving and giving,\n" +
+                "Saturday's child works hard for a living,\n" +
+                "But the child born on the Sabbath Day,\n" +
+                "Is fair and wise and good in every way.\n");
+            System.out.println("What month were you born?");
+            month= keyboard.nextInt();
+            System.out.println("What day were you born?");
+            day=  keyboard.nextInt();
+            System.out.println("What year were you born?");
+            year=  keyboard.nextInt();
+
+            daysofmonth(month);
     }
-
-
-    public static int monthoftheyear(int month) {
-        if (month == 1) {
-            System.out.println("January which is the first month of the year");
-        }
-        if (month == 2) {
-            System.out.println("February which is the second month of they year");
-        }
-        if (month == 3) {
-            System.out.println("March which is the third month of the year");
-        } {
-
-        }
-
     }
+      public static int daysofmonth(int month){
+        String monthString;
+        switch (month){
+            case 1: monthString= "There are 31 days in January";
+            case 2: monthString= "There are either 28 days or 29 days in February";
+            case 3: monthString= "There are 31 days in March";
+            case 4: monthString= "There are 30 days in April";
+            case 5: monthString= "There are 31 days in May";
+            case 6: monthString= "There are 30 days in June";
+            case 7: monthString= "There are 31 days in July";
+            case 8: monthString= "There are 30 days in August";
+            case 9: monthString= "There are 31 days in September";
+            case 10: monthString= "There are 30 days in October";
+            case 11: monthString= "There are 31 days in November";
+            case 12: monthString= "There are 30 days in December";
+
+        }
+      }
 }
 
 
