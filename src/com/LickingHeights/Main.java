78456237d4c4dece65c5e89b9e9ad16a94233e4c
Karen <karen.bandong.2018@student.lhschools.org>
2019-04-01 20:31:19 -0400

    method 1 done

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -28,25 +28,39 @@ public class Main {
             year=  keyboard.nextInt();
 
             daysofmonth(month);
+           
     }
     }
-      public static int daysofmonth(int month){
-        String monthString;
+      public static String daysofmonth(int month){
+        String monthString="";
         switch (month){
             case 1: monthString= "There are 31 days in January";
+                break;
             case 2: monthString= "There are either 28 days or 29 days in February";
+                break;
             case 3: monthString= "There are 31 days in March";
+                break;
             case 4: monthString= "There are 30 days in April";
+                break;
             case 5: monthString= "There are 31 days in May";
+                break;
             case 6: monthString= "There are 30 days in June";
+                break;
             case 7: monthString= "There are 31 days in July";
+                break;
             case 8: monthString= "There are 30 days in August";
+                break;
             case 9: monthString= "There are 31 days in September";
+                break;
             case 10: monthString= "There are 30 days in October";
+                break;
             case 11: monthString= "There are 31 days in November";
+                break;
             case 12: monthString= "There are 30 days in December";
-
+                break;
         }
+
+        return (monthString);
       }
 }
 
